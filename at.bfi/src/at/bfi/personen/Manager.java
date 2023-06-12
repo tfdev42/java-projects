7802cc57f1e5413e734d53260f24f5442690defd
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-06-12 20:59:57 +0200

    nb

--- at.bfi/src/at/bfi/personen/Manager.java
+++ at.bfi/src/at/bfi/personen/Manager.java
@@ -6,14 +6,45 @@ public class Manager {
         Person p1 = new Person();
         System.out.println("Name ist: " + p1.getName());
         p1.setName("Tom");
-        System.out.println("Name ist: " + p1.getName());
+        System.out.println("Name ist: " + p1.getName() + " hat " + p1.getAnzTaenze() + " taenze");
 
         Person p2 = new Person();
         p2.setName("Ida");
         System.out.println("Name ist: " + p2.getName());
+        
 
         Person p3 = new Person("Person3");
         System.out.println("Name ist: " + p3.getName());
+
+        System.out.println(p3);
+
+
+        // Array von personen (10 personen)
+
+        Person[] gruppe = new Person[10];
+
+        for (int i=0; i<gruppe.length; i++){
+            gruppe[i] = new Person("Person" + (i+1));
+            System.out.println(gruppe[i]);
+        }
+
+        for (int i=0; i<gruppe.length; i++){
+            int number = i+1;
+            if (number % 2 != 0){
+                //gruppe[i].setAnzTaenze(30);
+                for (int t=1; t<=30; t++){
+                    gruppe[i].tanzen();
+                }
+                
+            }
+        }
+        
+        for (int i=0; i<gruppe.length; i++){
+            System.out.println(gruppe[i]);
+        }
+
+
+
         
     }
 }
--- at.bfi/src/at/bfi/personen/Person.java
+++ at.bfi/src/at/bfi/personen/Person.java
@@ -3,18 +3,24 @@ package at.bfi.personen;
 public class Person {
     
     private String name;
+    private int anzTaenze;
 
     
-
+    // Constructors
     public Person() {
         this.setName("Unbekannt");
+        this.anzTaenze = 0;
     }
 
     public Person(String name) {
         //this.name = name;
         this.setName(name);
+        this.anzTaenze = 0;
     }
 
+
+
+    // Setters Getters
     public String getName() {
         return this.name;
     }
@@ -23,6 +29,25 @@ public class Person {
         this.name = name;
     }
 
+    public int getAnzTaenze() {
+        return this.anzTaenze;
+    }
+
+    public void setAnzTaenze(int anzTaenze){
+        this.anzTaenze = anzTaenze;
+    }
     
 
+    // METHODS
+    public void tanzen() {
+        this.anzTaenze++;
+        
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s hat %d mal getanzt", name, anzTaenze);
+    }
+
+    
 }
