dd5a8910d63b46ac67f5f5283ac1029c58b30704
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-06-13 22:37:10 +0200

    dt

--- exercises/inheritance/cars/Car.java
+++ exercises/inheritance/cars/Car.java
@@ -1,5 +1,22 @@
 package inheritance.cars;
 
 public class Car extends Vehicle{
+
+    private int horsePower;
+
+    public Car(int tires, int seats, int motor, int horsePower) {
+        super(tires, seats, motor);
+        this.setHorsePower(horsePower);;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+    
+    
     
 }
--- exercises/inheritance/cars/Vehicle.java
+++ exercises/inheritance/cars/Vehicle.java
@@ -1,5 +1,35 @@
 package inheritance.cars;
 
 public class Vehicle {
+
+    private int tires;
+    private int seats;
+    private int motor;
+
+    public Vehicle() {
+    }
+    public Vehicle(int tires, int seats, int motor) {
+        this.setTires(tires);
+        this.setSeats(seats);
+        this.setMotor(motor);
+    }
+    public int getTires() {
+        return tires;
+    }
+    public int getSeats() {
+        return seats;
+    }
+    public int getMotor() {
+        return motor;
+    }
+    public void setTires(int tires) {
+        this.tires = tires;
+    }
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+    public void setMotor(int motor) {
+        this.motor = motor;
+    }
     
 }
