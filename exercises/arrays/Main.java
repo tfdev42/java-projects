bb89a764f91b9c77d76f98ba3c9d23528b6f51e9
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-06-17 15:10:18 +0200

    dt

---
+++ exercises/arrays/Main.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        int[] myIntArray = new int[10];
+
+        double[] myDoubleArray = new double[10];
+        
+    }
+}
--- exercises/inheritance/cars/Mercedes.java
+++ exercises/inheritance/cars/Mercedes.java
@@ -1,5 +1,15 @@
 package inheritance.cars;
 
 public class Mercedes extends Car{
+
+    private String type = "Mercedes";
+
+    public Mercedes(int tires, int seats, int motor, int horsePower, String type) {
+        super(tires, seats, motor, horsePower);
+        this.type = type;
+    }
+
+    
+
     
 }
