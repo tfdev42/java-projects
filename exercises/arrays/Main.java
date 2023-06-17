32a1250301d6d3984597bbbe11985c22e4345f17
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-06-17 20:10:47 +0200

    dt

--- exercises/arrays/Main.java
+++ exercises/arrays/Main.java
@@ -50,13 +50,21 @@ public class Main {
         System.out.println();
 
         getRandomArray(5);
+
+        int[] randArray = getRandomArray(10);
+        System.out.println(Arrays.toString(randArray));
+        Arrays.sort(randArray);
+        System.out.println(Arrays.toString(randArray));
         
     }
 
     private static int[] getRandomArray(int len) {
          Random random = new Random();
          int[] newInt = new int[len];
-         
+         for (int i = 0; i < len; i++){
+            newInt[i] = random.nextInt(100);
+         }
+
          return newInt;
     }
 }
