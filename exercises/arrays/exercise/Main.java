9d8030877725fdfa1aff509235d4fa774e1e880c
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-06-28 17:43:30 +0200

    dt

---
+++ exercises/arrays/exercise/Main.java
@@ -0,0 +1,8 @@
+package arrays.exercise;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        SortedArray.printArray(SortedArray.sortIntegers(SortedArray.getIntegers(5)));
+    }
+}
