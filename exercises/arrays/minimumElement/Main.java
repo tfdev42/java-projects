b8193fb7468114ae53d7d1deec0053e2c19bac26
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-07-03 10:31:37 +0200

    dt

--- exercises/arrays/minimumElement/Main.java
+++ exercises/arrays/minimumElement/Main.java
@@ -37,7 +37,6 @@ public class Main {
 
     public static int findMin(int[] array){
         Arrays.sort(array);
-        int minimum = array[0];
-        return minimum;
+        return array[0];
     }
 }
