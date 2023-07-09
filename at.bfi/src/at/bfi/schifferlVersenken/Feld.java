a18a977878175047d2b949c1a27b2e6947924939
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-07-09 20:45:29 +0200

    nb

--- at.bfi/src/at/bfi/schifferlVersenken/Feld.java
+++ at.bfi/src/at/bfi/schifferlVersenken/Feld.java
@@ -12,4 +12,51 @@ public class Feld {
     // +hatEinSchiff():boolean
     // +wurdeGetroffen():boolean
     // +toString():String
+
+    private boolean frei;
+    private boolean treffer;
+
+    public Feld() {
+        this.frei = true;
+        this.treffer = false;
+    }
+
+    public void setSchiff(){
+        if (this.frei){
+            this.frei = false;
+            this.treffer = false;
+        }
+    }
+
+    public boolean istFeldFrei(){
+        return this.frei;
+    }
+
+    public boolean hatEinSchiff(){
+        return this.frei == false;
+    }
+
+    public boolean hatTreffer(){
+        return this.treffer;
+    }
+
+    public void wurdeGetroffen(){
+        this.treffer = true;
+    }
+
+    @Override
+    public String toString(){
+        // frei: (Leerzeichen)
+        // hat schiff und nicht getroffen: *
+        // hat schiff und ist getroffen: X
+        if (this.istFeldFrei()){
+            return " ";
+        }
+        else if (this.hatTreffer()){
+            return "X";
+        }
+        else return "*";
+    }
+
+    
 }
