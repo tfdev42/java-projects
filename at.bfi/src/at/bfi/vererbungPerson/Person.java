0e34da9383bd114d4df961614a02d2af4c67dda2
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-07-19 19:12:37 +0200

    nb

---
+++ at.bfi/src/at/bfi/vererbungPerson/Person.java
@@ -0,0 +1,33 @@
+package at.bfi.vererbungPerson;
+
+public class Person {
+    private String vName;
+    private String nName;
+
+    public Person(String vName, String nName){
+        this.nName = nName;
+        this.vName = vName;
+    }
+
+    public void setvName(String vName) {
+        this.vName = vName;
+    }
+
+    public void setnName(String nName) {
+        this.nName = nName;
+    }
+
+    public String getvName() {
+        return this.vName;
+    }
+
+    public String getnName() {
+        return this.nName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [vName=" + this.vName + ", nName=" + this.nName + "]";
+    }
+    
+}
