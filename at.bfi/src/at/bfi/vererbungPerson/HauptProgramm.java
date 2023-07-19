17939fa9431541e6f79e2d13191ece092e0f61fe
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-07-19 20:58:00 +0200

    nb

--- at.bfi/src/at/bfi/vererbungPerson/HauptProgramm.java
+++ at.bfi/src/at/bfi/vererbungPerson/HauptProgramm.java
@@ -14,6 +14,35 @@ public class HauptProgramm {
 
         Person susi2 = susi;
         System.out.println(susi2);
+        System.out.println();
+
+        Mitarbeiter daniel = new Mitarbeiter("Daniel", "Weber", "Vinothek");
+        System.out.println(daniel);
+        System.out.println();
+
+        // Verwalten von Obj der Super- und Subklassen in einem gemeinsamen Array
+        Person[] alle = new Person[3];
+        alle[0] = daniel;
+        alle[1] = susi;
+        alle[2] = p1;
+
+        // Alle Persons im Array ausgeben
+        // for (Person person : alle) {
+        //     System.out.println(person);            
+        // }
+        for (int i=0; i<alle.length; i++){
+            System.out.print(alle[i].getvName());
+
+            if (alle[i] instanceof Mitarbeiter){
+                System.out.print(" ist ein MA der Abteilung ");
+                Mitarbeiter m = (Mitarbeiter)alle[i];
+                System.out.println(m.getAbteilung());
+            }else if (alle[i] instanceof Kunde){
+                System.out.print(" ist ein Kunde mit der Kundennummer ");
+                Kunde k = (Kunde)alle[i];
+                System.out.println(k.getKundeID());
+            }else System.out.println(" ist eine Person");
+        }
 
 
     }
