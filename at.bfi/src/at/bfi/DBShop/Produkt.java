00aee1e4d9a59aa8ad3ef747be220f0c293b332e
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-07-31 19:08:42 +0200

    nb

---
+++ at.bfi/src/at/bfi/DBShop/Produkt.java
@@ -0,0 +1,29 @@
+package at.bfi.DBShop;
+
+public class Produkt {
+    private long produktID;
+    private String bezeichnung;
+    private float nettopreis;
+    private long produktgruppe;
+
+
+    public Produkt(long ProduktID, String Bezeichnung, float Nettopreis, long Produktgruppe) {
+        produktID = ProduktID;
+        bezeichnung = Bezeichnung;
+        nettopreis = Nettopreis;
+        produktgruppe = Produktgruppe;
+    }
+
+    public Produkt(String Bezeichnung, float Nettopreis, long Produktgruppe){
+        this(0, Bezeichnung, Nettopreis, Produktgruppe);
+    }
+
+    public String toString(){
+        return bezeichnung + " kostet " + nettopreis
+            + " Euro netto und gehoert zur Produktgruppe " + produktgruppe
+            + " (Produkt-Nr.: " + produktID + ")";
+    }
+
+    
+    
+}
---
+++ at.bfi/src/at/bfi/DBShop/ProduktManager.java
@@ -0,0 +1,51 @@
+package at.bfi.DBShop;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProduktManager {
+
+    private Connection connection;
+
+    public ProduktManager(){
+        try {
+            DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+            String connectionUrl =
+                "jdbc:sqlserver://localhost;"
+                + "database=shop;"
+                + "user=bfi;"
+                + "password=bfi;"
+                + "encrypt=false;";
+
+            this.connection = DriverManager.getConnection(connectionUrl);
+
+        } catch (SQLException ex) {
+            this.connection = null;
+            
+        }
+    }
+
+
+
+    public ArrayList<Produkt> getAlleProdukte(){
+
+        ArrayList<Produkt> result = new ArrayList<Produkt>();
+
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT FROM Proodukt ORDER BY Bezeichnung");
+            Produkt p;
+
+            while (rs.next()){
+                p = new Produkt(rs.getLong("ProduktID"), rs.getString("Bezeichnung"), rs.getFloat("Nettopreis"), rs.getLong("FKProduktgruppe"));
+                result.add(p);
+            }
+            
+        } catch (SQLException se) {
+            // TODO: handle exception
+        }
+        return result;
+        
+    }
+    
+}
