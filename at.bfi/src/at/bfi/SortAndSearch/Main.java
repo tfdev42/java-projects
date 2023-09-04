9b3e1fe7e92c505fd8bcf2e4d4956dca17bb8926
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-09-04 21:17:19 +0200

    nb

--- at.bfi/src/at/bfi/SortAndSearch/Main.java
+++ at.bfi/src/at/bfi/SortAndSearch/Main.java
@@ -2,6 +2,29 @@ package at.bfi.SortAndSearch;
 
 public class Main {
     public static void main(String[] args) {
-        
+        // 1 dimensional int array mit initialisierung
+        int[] zahlen = new int[]{8, 98, 67, 4, 3, 100, 23, 456};
+
+        // Sortiere die Zahlen in Array mit BubbleSort
+        System.out.println("Vorher (unsortiert):");
+        Ausgabe(zahlen);
+        System.out.println();
+        System.out.println("Nachher (sortiert):");
+        Ausgabe(zahlen);
+
+
     }
+
+    public static void Ausgabe(int[] zahlen){
+        //gibt alle Elemente mit Komma getrennt aus
+        for (int i = 0; i < zahlen.length; i++){
+            System.out.print(zahlen[i]);
+            // merken >>>>
+            if (i < zahlen.length-1){
+                System.out.print(", ");
+            }
+        }
+    }
+
+
 }
--- at.bfi/src/at/bfi/SortAndSearch/Sort.java
+++ at.bfi/src/at/bfi/SortAndSearch/Sort.java
@@ -2,4 +2,7 @@ package at.bfi.SortAndSearch;
 
 public class Sort {
     
+    public static void BubbleSort()
+
+
 }
