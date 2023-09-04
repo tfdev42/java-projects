6604510b860f67a71244a3a311a773cda58f8a95
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-09-04 20:49:43 +0200

    nb

---
+++ at.bfi/src/at/bfi/SortAndSearch/Main.java
@@ -0,0 +1,7 @@
+package at.bfi.SortAndSearch;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ at.bfi/src/at/bfi/SortAndSearch/Sort.java
@@ -0,0 +1,5 @@
+package at.bfi.SortAndSearch;
+
+public class Sort {
+    
+}
